6e17f5a342caafb68abb97e4ba6573377b4bef41
Soheil Mamdouhi <soheil.mamdouhi@pooya.ir>
2022-10-30 16:29:09 +0330

    Commit new version

--- Default.aspx
+++ Default.aspx
@@ -6,7 +6,7 @@
         <div class="col-md-6">
             <h2>Servers</h2>
             <p>
-                <asp:GridView ID="dgridProjects" runat="server" CellPadding="4" ForeColor="#333333" OnSelectedIndexChanged="dgridProjects_SelectedIndexChanged">
+                <asp:GridView ID="dgridShowData" runat="server" CellPadding="4" ForeColor="#333333" OnSelectedIndexChanged="dgridShowData_SelectedIndexChanged">
                     <AlternatingRowStyle BackColor="White" />
                     <Columns>
                         <asp:CommandField SelectImageUrl="~/Images/checkmark.png" ShowSelectButton="True" />
@@ -21,7 +21,7 @@
                 <asp:Label ID="lblProjectName" runat="server" Visible="False"></asp:Label>
             </p>
             <p>
-                <asp:GridView ID="dgridShowData" runat="server" CellPadding="4" ForeColor="#333333" OnSelectedIndexChanged="dgridShowData_SelectedIndexChanged">
+                <asp:GridView ID="dgridShowVersions" runat="server" CellPadding="4" ForeColor="#333333">
                     <AlternatingRowStyle BackColor="White" />
                     <Columns>
                         <asp:CommandField SelectImageUrl="~/Images/checkmark.png" ShowSelectButton="True" />
@@ -36,42 +36,6 @@
             </p>
         </div>
         <div class="col-md-6">
-            <h2>Server Data</h2>
-            <p>
-                <asp:Label ID="lblStatus" runat="server" Font-Bold="True" Font-Size="Larger" ForeColor="Red" Text="Not Set"></asp:Label>
-            </p>
-            <p>
-                <asp:Label ID="lblID" runat="server" Font-Bold="True" Text="ID: "></asp:Label>
-                <asp:Label ID="lblIDData" runat="server"></asp:Label>
-            </p>
-            <p>
-                <asp:Label ID="lblServerName" runat="server" Font-Bold="True" Text="Server Name: "></asp:Label>
-                <asp:Label ID="lblServerNameData" runat="server"></asp:Label>
-            </p>
-            <p>
-                <asp:Label ID="lblIPAddress" runat="server" Font-Bold="True" Text="IP Address:  "></asp:Label>
-                <asp:Label ID="lblIPAddressData" runat="server"></asp:Label>
-            </p>
-            <p>
-                <asp:Label ID="lblTemp" runat="server" Visible="False"></asp:Label>
-            </p>
-            <p>
-                <asp:Label ID="lblMessages" runat="server" Font-Bold="True" Text="Messages:"></asp:Label>
-&nbsp;
-                <asp:Label ID="lblMessageData01" runat="server" ForeColor="Red" style="text-align: right;"></asp:Label>
-            </p>
-            <p>
-                <asp:Label ID="lblMessageData02" runat="server" ForeColor="Red"></asp:Label>
-            </p>
-            <p>
-                &nbsp;<asp:Button ID="btnRead" runat="server" Text="Show Log" OnClick="btnRead_Click" Width="130px" Enabled="False" />
-            &nbsp;&nbsp;&nbsp;&nbsp;<asp:Button ID="btnDownloadLog" runat="server" Enabled="False" OnClick="btnDownloadLog_Click" Text="Download Log" Width="130px" />
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                <asp:Button ID="btnDownloadFull" runat="server" OnClick="btnDownload_Click" Text="Download Log File" Width="130px" Enabled="False" />
-            </p>
-            <p>
-                <asp:TextBox ID="txtOutput" runat="server" Font-Size="Small" Height="500px" ReadOnly="True" TextMode="MultiLine" Width="100%" Wrap="False"></asp:TextBox>
-            </p>
         </div>
     </div>
 </asp:Content>
--- Default.aspx.cs
+++ Default.aspx.cs
@@ -4,9 +4,18 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
 
 public partial class _Default : Page
 {
+    public String strWeblogic11gJarPath = @"E:\Weblogic\Weblogic11g\wlserver_10.3\server\lib\weblogic.jar";
+    public String strWeblogic12cJarPath = @"E:\Weblogic\Weblogic12c\wlserver\server\lib\weblogic.jar";
+    public String strJythonFilespath = @"E:\Weblogic\RetriveAppsVersions.py";
+    public String[] stringArr = new String[5];
+
     protected void Page_Load(object sender, EventArgs e)
     {
         if (!IsPostBack)
@@ -16,28 +25,74 @@ public partial class _Default : Page
         }
     }
 
-    protected void dgridProjects_SelectedIndexChanged(object sender, EventArgs e)
-    {
-
-    }
-
-    protected void btnRead_Click(object sender, EventArgs e)
-    {
-
-    }
-
-    protected void btnDownloadLog_Click(object sender, EventArgs e)
-    {
-
-    }
-
     protected void dgridShowData_SelectedIndexChanged(object sender, EventArgs e)
     {
+        DataTable objDataTable = new DataTable();
+        String strCommandToExecute = "";
+        String strOutputFileName = Guid.NewGuid().ToString() + ".tmp";
+        String strOutputFileContent;
+        objDataTable = clsServersManager.SelectServersByID(dgridShowData.SelectedRow.Cells[1].Text);
 
+        stringArr = objDataTable.Rows[0].ItemArray.Select(arrServer => arrServer.ToString()).ToArray();
+
+        if (stringArr[6] == "11g")
+        {
+            strCommandToExecute = "/C java -classpath " + strWeblogic11gJarPath + " weblogic.WLST " + strJythonFilespath + " " + stringArr[4] + " " + stringArr[5] + " t3://" + stringArr[2] + ":" + stringArr[3] + @" > E:\Weblogic\" + strOutputFileName;
+            Process.Start("cmd.exe", strCommandToExecute);
+        }
+        else
+        {
+            strCommandToExecute = "/C java -classpath " + strWeblogic12cJarPath + " weblogic.WLST " + strJythonFilespath + " " + stringArr[4] + " " + stringArr[5] + " t3://" + stringArr[2] + ":" + stringArr[3] + @" > E:\Weblogic\" + strOutputFileName;
+            Process.Start("cmd.exe", strCommandToExecute);
+        }
+
+        Thread.Sleep(30000);
+
+        strOutputFileContent = File.ReadAllText(@"E:\Weblogic\" + strOutputFileName);
+        strOutputFileContent = strOutputFileContent.Remove(0, strOutputFileContent.IndexOf("["));
+
+        string[] splitted = strOutputFileContent.Split(new string[] { System.Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+        File.Delete(@"E:\Weblogic\" + strOutputFileName);
     }
 
-    protected void btnDownload_Click(object sender, EventArgs e)
-    {
 
-    }
+
+
+
+
+
+
+
+
+    //DataTable objDataTable = new DataTable();
+
+    //objDataTable = clsServersDataManager.SelectServer(dgridShowData.SelectedRow.Cells[1].Text);
+
+    //    stringArr = objDataTable.Rows[0].ItemArray.Select(arrServer => arrServer.ToString()).ToArray();
+
+    //    using (var ssh = new SshClient(stringArr[2], stringArr[5], stringArr[6]))
+    //    {
+    //        ssh.Connect();
+
+    //        lblTemp.Text = ssh.CreateCommand("wc -l " + stringArr[3] + " | awk '{print $1}'").Execute();
+
+    //        ssh.Disconnect();
+    //    }
+
+    //    lblIDData.Text = dgridShowData.SelectedRow.Cells[1].Text;
+    //    lblServerNameData.Text = dgridShowData.SelectedRow.Cells[2].Text;
+    //    lblIPAddressData.Text = dgridShowData.SelectedRow.Cells[3].Text;
+
+    //    btnRead.Enabled = true;
+    //    btnDownloadFull.Enabled = true;
+    //    btnDownloadLog.Enabled = false;
+    //    lblStatus.Text = "Set";
+    //    lblStatus.ForeColor = System.Drawing.Color.Green;
+
+    //    lblMessageData01.Text = "";
+    //    lblMessageData02.Text = "";
+    //    txtOutput.Text = "";
+
+
 }
